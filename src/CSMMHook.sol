f381f01773f4f820b131219ac0396309976f3791
jen <60794961+jennyg0@users.noreply.github.com>
2025-06-12 18:30:23 -0400

    fmt

--- src/CSMMHook.sol
+++ src/CSMMHook.sol
@@ -31,104 +31,63 @@ contract CSMMHook is BaseHook {
 
     constructor(IPoolManager poolManager) BaseHook(poolManager) {}
 
-    function getHookPermissions()
-        public
-        pure
-        override
-        returns (Hooks.Permissions memory)
-    {
-        return
-            Hooks.Permissions({
-                beforeInitialize: false,
-                afterInitialize: false,
-                beforeAddLiquidity: true, // Don't allow adding liquidity normally
-                afterAddLiquidity: false,
-                beforeRemoveLiquidity: false,
-                afterRemoveLiquidity: false,
-                beforeSwap: true, // Override how swaps are done
-                afterSwap: false,
-                beforeDonate: false,
-                afterDonate: false,
-                beforeSwapReturnDelta: true, // Allow beforeSwap to return a custom delta
-                afterSwapReturnDelta: false,
-                afterAddLiquidityReturnDelta: false,
-                afterRemoveLiquidityReturnDelta: false
-            });
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: true, // Don't allow adding liquidity normally
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: true, // Override how swaps are done
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: true, // Allow beforeSwap to return a custom delta
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
     }
 
     // Disable adding liquidity through the PM
-    function _beforeAddLiquidity(
-        address,
-        PoolKey calldata,
-        ModifyLiquidityParams calldata,
-        bytes calldata
-    ) internal pure override returns (bytes4) {
+    function _beforeAddLiquidity(address, PoolKey calldata, ModifyLiquidityParams calldata, bytes calldata)
+        internal
+        pure
+        override
+        returns (bytes4)
+    {
         revert AddLiquidityThroughHook();
     }
 
     function addLiquidity(PoolKey calldata key, uint256 amountEach) external {
-        poolManager.unlock(
-            abi.encode(
-                CallbackData(
-                    amountEach,
-                    key.currency0,
-                    key.currency1,
-                    msg.sender
-                )
-            )
-        );
+        poolManager.unlock(abi.encode(CallbackData(amountEach, key.currency0, key.currency1, msg.sender)));
     }
 
-    function _beforeSwap(
-        address,
-        PoolKey calldata key,
-        SwapParams calldata params,
-        bytes calldata
-    ) internal override returns (bytes4, BeforeSwapDelta, uint24) {
-        uint256 amountInOutPositive = params.amountSpecified > 0
-            ? uint256(params.amountSpecified)
-            : uint256(-params.amountSpecified);
+    function _beforeSwap(address, PoolKey calldata key, SwapParams calldata params, bytes calldata)
+        internal
+        override
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        uint256 amountInOutPositive =
+            params.amountSpecified > 0 ? uint256(params.amountSpecified) : uint256(-params.amountSpecified);
 
-        BeforeSwapDelta beforeSwapDelta = toBeforeSwapDelta(
-            int128(-params.amountSpecified),
-            int128(params.amountSpecified)
-        );
+        BeforeSwapDelta beforeSwapDelta =
+            toBeforeSwapDelta(int128(-params.amountSpecified), int128(params.amountSpecified));
 
         if (params.zeroForOne) {
-            key.currency0.take(
-                poolManager,
-                address(this),
-                amountInOutPositive,
-                true
-            );
+            key.currency0.take(poolManager, address(this), amountInOutPositive, true);
 
-            key.currency1.settle(
-                poolManager,
-                address(this),
-                amountInOutPositive,
-                true
-            );
+            key.currency1.settle(poolManager, address(this), amountInOutPositive, true);
         } else {
-            key.currency0.settle(
-                poolManager,
-                address(this),
-                amountInOutPositive,
-                true
-            );
-            key.currency1.take(
-                poolManager,
-                address(this),
-                amountInOutPositive,
-                true
-            );
+            key.currency0.settle(poolManager, address(this), amountInOutPositive, true);
+            key.currency1.take(poolManager, address(this), amountInOutPositive, true);
         }
 
         return (this.beforeSwap.selector, beforeSwapDelta, 0);
     }
 
-    function unlockCallback(
-        bytes calldata data
-    ) external onlyPoolManager returns (bytes memory) {
+    function unlockCallback(bytes calldata data) external onlyPoolManager returns (bytes memory) {
         CallbackData memory callbackData = abi.decode(data, (CallbackData));
 
         callbackData.currency0.settle(
@@ -137,12 +96,7 @@ contract CSMMHook is BaseHook {
             callbackData.amountEach,
             false // `burn` = `false` i.e. we're actually transferring tokens, not burning ERC-6909 Claim Tokens
         );
-        callbackData.currency1.settle(
-            poolManager,
-            callbackData.sender,
-            callbackData.amountEach,
-            false
-        );
+        callbackData.currency1.settle(poolManager, callbackData.sender, callbackData.amountEach, false);
 
         callbackData.currency0.take(
             poolManager,
@@ -150,12 +104,7 @@ contract CSMMHook is BaseHook {
             callbackData.amountEach,
             true // true = mint claim tokens for the hook, equivalent to money we just deposited to the PM
         );
-        callbackData.currency1.take(
-            poolManager,
-            address(this),
-            callbackData.amountEach,
-            true
-        );
+        callbackData.currency1.take(poolManager, address(this), callbackData.amountEach, true);
 
         return "";
     }
--- test/CSMMHook.t.sol
+++ test/CSMMHook.t.sol
@@ -27,26 +27,16 @@ contract CSMMHookTest is Test, Deployers {
         (currency0, currency1) = deployMintAndApprove2Currencies();
 
         address hookAddress = address(
-            uint160(
-                Hooks.BEFORE_ADD_LIQUIDITY_FLAG |
-                    Hooks.BEFORE_SWAP_FLAG |
-                    Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG
-            )
+            uint160(Hooks.BEFORE_ADD_LIQUIDITY_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG)
         );
         deployCodeTo("CSMMHook.sol", abi.encode(manager), hookAddress);
         hook = CSMMHook(hookAddress);
 
-        (key, ) = initPool(currency0, currency1, hook, 3000, SQRT_PRICE_1_1);
+        (key,) = initPool(currency0, currency1, hook, 3000, SQRT_PRICE_1_1);
 
         // Add some initial liquidity through the custom `addLiquidity` function
-        IERC20Minimal(Currency.unwrap(key.currency0)).approve(
-            hookAddress,
-            1000 ether
-        );
-        IERC20Minimal(Currency.unwrap(key.currency1)).approve(
-            hookAddress,
-            1000 ether
-        );
+        IERC20Minimal(Currency.unwrap(key.currency0)).approve(hookAddress, 1000 ether);
+        IERC20Minimal(Currency.unwrap(key.currency1)).approve(hookAddress, 1000 ether);
 
         hook.addLiquidity(key, 1000e18);
     }
@@ -55,12 +45,7 @@ contract CSMMHookTest is Test, Deployers {
         vm.expectRevert();
         modifyLiquidityRouter.modifyLiquidity(
             key,
-            ModifyLiquidityParams({
-                tickLower: -60,
-                tickUpper: 60,
-                liquidityDelta: 1e18,
-                salt: bytes32(0)
-            }),
+            ModifyLiquidityParams({tickLower: -60, tickUpper: 60, liquidityDelta: 1e18, salt: bytes32(0)}),
             ZERO_BYTES
         );
     }
@@ -69,69 +54,51 @@ contract CSMMHookTest is Test, Deployers {
         // We add 1000 * (10^18) of liquidity of each token to the CSMM pool
         // The actual tokens will move into the PM
         // But the hook should get equivalent amount of claim tokens for each token
-        uint token0ClaimID = CurrencyLibrary.toId(currency0);
-        uint token1ClaimID = CurrencyLibrary.toId(currency1);
+        uint256 token0ClaimID = CurrencyLibrary.toId(currency0);
+        uint256 token1ClaimID = CurrencyLibrary.toId(currency1);
 
-        uint token0ClaimsBalance = manager.balanceOf(
-            address(hook),
-            token0ClaimID
-        );
-        uint token1ClaimsBalance = manager.balanceOf(
-            address(hook),
-            token1ClaimID
-        );
+        uint256 token0ClaimsBalance = manager.balanceOf(address(hook), token0ClaimID);
+        uint256 token1ClaimsBalance = manager.balanceOf(address(hook), token1ClaimID);
 
         assertEq(token0ClaimsBalance, 1000e18);
         assertEq(token1ClaimsBalance, 1000e18);
     }
 
     function test_swap_exactInput_zeroForOne() public {
-        PoolSwapTest.TestSettings memory settings = PoolSwapTest.TestSettings({
-            takeClaims: false,
-            settleUsingBurn: false
-        });
+        PoolSwapTest.TestSettings memory settings =
+            PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false});
 
         // Swap exact input 100 Token A
-        uint balanceOfTokenABefore = key.currency0.balanceOfSelf();
-        uint balanceOfTokenBBefore = key.currency1.balanceOfSelf();
+        uint256 balanceOfTokenABefore = key.currency0.balanceOfSelf();
+        uint256 balanceOfTokenBBefore = key.currency1.balanceOfSelf();
         swapRouter.swap(
             key,
-            SwapParams({
-                zeroForOne: true,
-                amountSpecified: -100e18,
-                sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
-            }),
+            SwapParams({zeroForOne: true, amountSpecified: -100e18, sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1}),
             settings,
             ZERO_BYTES
         );
-        uint balanceOfTokenAAfter = key.currency0.balanceOfSelf();
-        uint balanceOfTokenBAfter = key.currency1.balanceOfSelf();
+        uint256 balanceOfTokenAAfter = key.currency0.balanceOfSelf();
+        uint256 balanceOfTokenBAfter = key.currency1.balanceOfSelf();
 
         assertEq(balanceOfTokenBAfter - balanceOfTokenBBefore, 100e18);
         assertEq(balanceOfTokenABefore - balanceOfTokenAAfter, 100e18);
     }
 
     function test_swap_exactOutput_zeroForOne() public {
-        PoolSwapTest.TestSettings memory settings = PoolSwapTest.TestSettings({
-            takeClaims: false,
-            settleUsingBurn: false
-        });
+        PoolSwapTest.TestSettings memory settings =
+            PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false});
 
         // Swap exact output 100 Token A
-        uint balanceOfTokenABefore = key.currency0.balanceOfSelf();
-        uint balanceOfTokenBBefore = key.currency1.balanceOfSelf();
+        uint256 balanceOfTokenABefore = key.currency0.balanceOfSelf();
+        uint256 balanceOfTokenBBefore = key.currency1.balanceOfSelf();
         swapRouter.swap(
             key,
-            SwapParams({
-                zeroForOne: true,
-                amountSpecified: 100e18,
-                sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
-            }),
+            SwapParams({zeroForOne: true, amountSpecified: 100e18, sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1}),
             settings,
             ZERO_BYTES
         );
-        uint balanceOfTokenAAfter = key.currency0.balanceOfSelf();
-        uint balanceOfTokenBAfter = key.currency1.balanceOfSelf();
+        uint256 balanceOfTokenAAfter = key.currency0.balanceOfSelf();
+        uint256 balanceOfTokenBAfter = key.currency1.balanceOfSelf();
 
         assertEq(balanceOfTokenBAfter - balanceOfTokenBBefore, 100e18);
         assertEq(balanceOfTokenABefore - balanceOfTokenAAfter, 100e18);
