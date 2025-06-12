08e2d024cdfc65405a9581a6662bc8e90ce86f3d
jen <60794961+jennyg0@users.noreply.github.com>
2025-06-12 08:59:41 -0400

    add csmm hook contract

---
+++ src/CSMMHook.sol
@@ -0,0 +1,162 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {Currency} from "v4-core/types/Currency.sol";
+import {CurrencySettler} from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.sol";
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+import {ModifyLiquidityParams, SwapParams} from "v4-core/types/PoolOperation.sol";
+
+// A CSMM is a pricing curve that follows the invariant `x + y = k`
+// instead of the invariant `x * y = k`
+
+// This is theoretically the ideal curve for a stablecoin or pegged pairs (stETH/ETH)
+// In practice, we don't usually see this in prod since depegs can happen and we dont want exact equal amounts
+// But is a nice little custom curve hook example
+
+contract CSMMHook is BaseHook {
+    using CurrencySettler for Currency;
+
+    error AddLiquidityThroughHook(); // error to throw when someone tries adding liquidity directly to the PoolManager
+
+    struct CallbackData {
+        uint256 amountEach;
+        Currency currency0;
+        Currency currency1;
+        address sender;
+    }
+
+    constructor(IPoolManager poolManager) BaseHook(poolManager) {}
+
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeAddLiquidity: true, // Don't allow adding liquidity normally
+                afterAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: true, // Override how swaps are done
+                afterSwap: false,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: true, // Allow beforeSwap to return a custom delta
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+
+    // Disable adding liquidity through the PM
+    function _beforeAddLiquidity(
+        address,
+        PoolKey calldata,
+        ModifyLiquidityParams calldata,
+        bytes calldata
+    ) internal pure override returns (bytes4) {
+        revert AddLiquidityThroughHook();
+    }
+
+    function addLiquidity(PoolKey calldata key, uint256 amountEach) external {
+        poolManager.unlock(
+            abi.encode(
+                CallbackData(
+                    amountEach,
+                    key.currency0,
+                    key.currency1,
+                    msg.sender
+                )
+            )
+        );
+    }
+
+    function _beforeSwap(
+        address,
+        PoolKey calldata key,
+        SwapParams calldata params,
+        bytes calldata
+    ) internal override returns (bytes4, BeforeSwapDelta, uint24) {
+        uint256 amountInOutPositive = params.amountSpecified > 0
+            ? uint256(params.amountSpecified)
+            : uint256(-params.amountSpecified);
+
+        BeforeSwapDelta beforeSwapDelta = toBeforeSwapDelta(
+            int128(-params.amountSpecified),
+            int128(params.amountSpecified)
+        );
+
+        if (params.zeroForOne) {
+            key.currency0.take(
+                poolManager,
+                address(this),
+                amountInOutPositive,
+                true
+            );
+
+            key.currency1.settle(
+                poolManager,
+                address(this),
+                amountInOutPositive,
+                true
+            );
+        } else {
+            key.currency0.settle(
+                poolManager,
+                address(this),
+                amountInOutPositive,
+                true
+            );
+            key.currency1.take(
+                poolManager,
+                address(this),
+                amountInOutPositive,
+                true
+            );
+        }
+
+        return (this.beforeSwap.selector, beforeSwapDelta, 0);
+    }
+
+    function unlockCallback(
+        bytes calldata data
+    ) internal returns (bytes memory) {
+        CallbackData memory callbackData = abi.decode(data, (CallbackData));
+
+        callbackData.currency0.settle(
+            poolManager,
+            callbackData.sender,
+            callbackData.amountEach,
+            false // `burn` = `false` i.e. we're actually transferring tokens, not burning ERC-6909 Claim Tokens
+        );
+        callbackData.currency1.settle(
+            poolManager,
+            callbackData.sender,
+            callbackData.amountEach,
+            false
+        );
+
+        callbackData.currency0.take(
+            poolManager,
+            address(this),
+            callbackData.amountEach,
+            true // true = mint claim tokens for the hook, equivalent to money we just deposited to the PM
+        );
+        callbackData.currency1.take(
+            poolManager,
+            address(this),
+            callbackData.amountEach,
+            true
+        );
+
+        return "";
+    }
+}
